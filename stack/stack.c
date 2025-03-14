ef9945b1d2e3d10653cf864498a0b238a56c337d
Kelvin Liu <kelvinyliu@gmail.com>
2025-03-14 16:35:18 +0000

    edited stack.c to implement proper data management, if out of scope make data still readable

--- stack/stack.c
+++ stack/stack.c
@@ -1,6 +1,7 @@
 #include "../node.c"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct stack {
 	int size;
@@ -20,49 +21,70 @@ struct stack* createStack() {
 	return _stack;
 }
 
-void appendStackItem(struct stack* stack, void* data) {
-	// create stack item;
+struct Node* createNodeItem(void* data, size_t dataSize) {
 	struct Node* newNode = malloc(sizeof(struct Node));
-	newNode->data = data;
+	if (newNode == NULL) {
+		printf("Error creating node item.\n");
+		return NULL;
+	}
+	newNode->data = malloc(dataSize);
+	if (newNode->data == NULL) {
+		printf("Error malloc data in node.\n");
+		return NULL;
+	}
+	memcpy(newNode->data, data, dataSize);
+	newNode->dataSize = dataSize;
 	newNode->next = NULL;
+	return newNode;
+}
 
-	if (stack->baseNode == NULL) {
+void appendStackItem(struct stack* stack, void* data, size_t dataSize) {
+	struct Node* newNode = createNodeItem(data, dataSize);
+	if (stack->baseNode == NULL || stack->size == 0) {
 		stack->baseNode = newNode;
 		stack->headNode = newNode;
 	} else {
-		struct Node* current = stack->baseNode;
-		while (current->next != NULL) {
-			current = current->next;
-		}
-		current->next = newNode;
+		stack->headNode->next = newNode;
 		stack->headNode = newNode;
 	}
-
 	stack->size++;
 }
 
 void* popStack(struct stack* stack) {
 	// if stack is empty
-	if (stack->headNode == NULL) {
+	if (stack->headNode == NULL || stack->size == 0) return NULL;
+
+	// get data from head node
+	void* data = malloc(stack->headNode->dataSize);
+	if (data == NULL) {
+		printf("Error popping head of stack.\n");
 		return NULL;
 	}
-	void* data = stack->headNode->data;
+	memcpy(data, stack->headNode->data, stack->headNode->dataSize);
+	
+	// if stack has one element
 	if (stack->baseNode == stack->headNode) {
-		free(stack->baseNode);
-		stack->baseNode = NULL;
-		stack->headNode = NULL;
-	} else {
-		struct Node* current = stack->baseNode;
-		while(current->next != stack->headNode) {
-			current = current->next;
-		}
-		current->next = NULL;
+		free(stack->headNode->data);
 		free(stack->headNode);
-		stack->headNode = current;
+		stack->headNode = NULL;
+		stack->baseNode = NULL;
+		stack->size--;
+		return data;
 	}
+
+	// get the node before head
+	struct Node* current = stack->baseNode;
+	while(current->next != stack->headNode) {
+		current = current->next;
+	}
+	free(stack->headNode->data);
+	free(stack->headNode);
+	current->next = NULL;
+	stack->headNode = current;
 	stack->size--;
 	return data;
 }
+
 void printStack(struct stack* stack) {
 	struct Node* current = stack->baseNode;
 
@@ -79,6 +101,7 @@ void freeStack(struct stack* stack) {
 	while (current != NULL) {
 		struct Node* temp = current;
 		current = current->next;
+		free(temp->data);
 		free(temp);
 	}
 	
