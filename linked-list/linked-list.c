c6a7c2d24e24ade749c70419dc54e1ec79724593
Kelvin Liu <kelvinyliu@gmail.com>
2025-03-15 22:49:44 +0000

    created new mthods in linked list to be more useful, such as get data from index and remove data/node from index

--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -1,13 +1,8 @@
-#include "../node.c"
+#include "linked-list.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-
-struct linkedList {
-	struct Node* baseNode;
-	int size;
-};
 
 struct linkedList* createLinkedList() {
 	struct linkedList* _linkedList = malloc(sizeof(struct linkedList));
@@ -52,6 +47,46 @@ void insertNode(struct linkedList* _linkedList, int data) {
     }
 
     _linkedList->size++;
+}
+
+struct Node* getLinkedListIndexNode(struct linkedList* _linkedList, int index) {
+	if (index < 0 || index >= _linkedList->size) {
+		printf("Index out of bound.\n");
+		return NULL;
+	}
+	struct Node* temp = _linkedList->baseNode;
+	for(int i = 0; i < index; i++){
+		temp = temp->next;
+	}
+
+	return temp;
+}
+
+void* getLinkedListIndexData(struct linkedList* _linkedList, int index) {
+	return getLinkedListIndexNode(_linkedList, index)->data;
+}
+
+void* removeLinkedListIndex(struct linkedList* _linkedList, int index) {
+	if (index < 0 || index >= _linkedList->size) {
+		printf("Cannot remove linked list item out of bound.\n");
+		return NULL;
+	}
+
+	struct Node* nodeBefore;
+	struct Node* nodeRemove;
+	// if index is 0
+	if (index == 0) {
+		nodeRemove = _linkedList->baseNode;
+		_linkedList->baseNode = nodeRemove->next;
+	} else {
+		nodeBefore = getLinkedListIndexNode(_linkedList, index-1);
+		nodeRemove = nodeBefore->next;
+		nodeBefore->next = nodeRemove->next;
+	}
+	void* returnData = nodeRemove->data;
+	free(nodeRemove);
+	_linkedList->size--;
+	return returnData;
 }
 
 bool searchLinkedList(struct linkedList* _linkedList, int searchValue) {
